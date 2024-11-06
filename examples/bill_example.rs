43fedc1788c2c529114c41872cd9e77973813a89
t-fbd <tairenfd@mailbox.org>
2024-11-06 12:33:38 -0500

    ex

--- examples/bill_example.rs
+++ examples/bill_example.rs
@@ -1,7 +1,7 @@
-use cdg_api::cdg_types::FormatType;
+use cdg_api::cdg_types::{BillType, FormatType};
 use cdg_api::endpoints::Endpoints;
-use cdg_api::param_models::BillListParams;
-use cdg_api::response_models::BillsResponse;
+use cdg_api::param_models::{BillActionsParams, BillListParams};
+use cdg_api::response_models::{BillActionsResponse, BillsResponse};
 use cdg_api::CongressApiClient;
 
 use std::error::Error;
@@ -10,23 +10,45 @@ fn main() -> Result<(), Box<dyn Error>> {
     let client = CongressApiClient::new(None)?; // Use default API key
 
     // Define parameters
-    let params = BillListParams::default().format(FormatType::Json).limit(10);
+    let params = BillActionsParams::default().format(FormatType::Json).limit(10);
 
     // Create the endpoint
-    let endpoint = Endpoints::BillList(params);
+    let endpoint = Endpoints::BillActions(118, BillType::S, 4361, params);
 
     // Fetch the data
-    let response: BillsResponse = client.fetch(endpoint)?;
+    let response: BillActionsResponse = client.fetch(endpoint)?;
 
     // Process the response
-    for bill in response.bills {
-        println!(
-            "{}, {}, {}\n",
-            bill.title.unwrap_or("".to_string()),
-            bill.bill_type.unwrap_or("".to_string()),
-            bill.number.unwrap_or("".to_string())
-        );
+    for action in response.actions {
+        println!("{:#?}\n", action);
+        println!("=====================");
+        println!("=====================\n");
     }
 
     Ok(())
 }
+
+//fn main() -> Result<(), Box<dyn Error>> {
+//    let client = CongressApiClient::new(None)?; // Use default API key
+//
+//    // Define parameters
+//    let params = BillListParams::default().format(FormatType::Json).limit(10);
+//
+//    // Create the endpoint
+//    let endpoint = Endpoints::BillList(params);
+//
+//    // Fetch the data
+//    let response: BillsResponse = client.fetch(endpoint)?;
+//
+//    // Process the response
+//    for bill in response.bills {
+//        println!(
+//            "{}, {}, {}\n",
+//            bill.title.unwrap_or("".to_string()),
+//            bill.bill_type.unwrap_or("".to_string()),
+//            bill.number.unwrap_or("".to_string())
+//        );
+//    }
+//
+//    Ok(())
+//}
